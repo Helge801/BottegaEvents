309507730b4682d93743f968a069b10717fe285e
JustimusPrime <hirejustin@yahoo.com>
2018-02-23 13:54:59 -0700

    4th styles

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -26,7 +26,14 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(event_params)
     @event.user_id = current_user.id
-    @event.save
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @event, notice: 'Event was successfully created' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def update
